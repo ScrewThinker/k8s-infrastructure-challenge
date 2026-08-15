6a7553c7241ebbc0b902f310b79f7514d8a2ac8e
ScrewThinker <pranaypandit282@gmail.com>
2026-08-15 12:26:27 +0530

    Make GitOps bootstrap idempotent

--- scripts/bootstrap-platform.sh
+++ scripts/bootstrap-platform.sh
@@ -10,13 +10,17 @@ fi
 
 kubectl config use-context "kind-${CLUSTER_NAME}"
 
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/ >/dev/null
-helm repo update >/dev/null
-helm upgrade --install metrics-server metrics-server/metrics-server \
-  --namespace kube-system \
-  --set args[0]=--kubelet-insecure-tls \
-  --set args[1]=--kubelet-preferred-address-types=InternalIP \
-  --wait --timeout 3m
+if ! helm status metrics-server --namespace kube-system >/dev/null 2>&1; then
+  helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/ >/dev/null
+  helm repo update >/dev/null
+  helm install metrics-server metrics-server/metrics-server \
+    --namespace kube-system \
+    --set args[0]=--kubelet-insecure-tls \
+    --set args[1]=--kubelet-preferred-address-types=InternalIP \
+    --wait --timeout 3m
+else
+  kubectl rollout status deployment/metrics-server --namespace kube-system --timeout=2m
+fi
 
 kubectl create namespace infra-challenge --dry-run=client -o yaml | kubectl apply -f -
 kubectl create secret generic challenge-app-secret \
